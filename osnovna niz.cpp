94e02256dedbb7ad2e82efb470464f8d86756733
nikorac <55491454+nikorac@users.noreply.github.com>
2019-10-02 13:36:45 +0200

    Add files via upload

---
+++ osnovna niz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int X[6]={2,34,1,67,99,7};
+	cout<<"A[0]="<<X[0]<<"  ";
+	cout<<"A[1]="<<X[1]<<"  ";
+	cout<<"A[2]="<<X[2]<<"  ";
+	cout<<"A[3]="<<X[3]<<"  ";
+	cout<<"A[4]="<<X[4]<<"  ";
+	cout<<"A[5]="<<X[5]<<"  ";
+	return 0;
+}
