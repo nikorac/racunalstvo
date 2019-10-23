582a9bc04e90f2141c57fb44a049e4b23b57cb7a
nikorac <55491454+nikorac@users.noreply.github.com>
2019-10-23 22:31:37 +0200

    Add files via upload

---
+++ array dz 2zad.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	// nisam znao da li mi trebamo napisati n ili korisnik pa sam izabro laksi nacin
+	int a;
+	int A[8]={0,1,2,3,4,5,6,7};
+	for(a=7;a>=0;a--)
+	{
+		cout<<"A["<<a<<"]="<<A[a]<<"  ";
+	}
+
+	
+system("PAUSE"); 
+return 0;
+}
